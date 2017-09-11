5e4c05540e4844943fbf7c1ca01b7347de943d5e
Felipe Thadeu <felipet65@gmail.com>
2017-09-11 16:05:33 -0300

    Exercício 019

---
+++ pythonTestes/Exercício 019.py
@@ -0,0 +1,4 @@
+import random
+a = int(input('Digite o número inicial de alunos: '))
+a2 = int(input('Digite o número final de alunos: '))
+print('O aluno escolhido foi: {}'.format(random.randint(a,a2)))

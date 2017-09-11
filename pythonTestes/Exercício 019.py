85cc27301ebed0c3dede100171488d6321b2f40e
Felipe Thadeu <felipet65@gmail.com>
2017-09-11 16:46:31 -0300

    Curso em Video

--- pythonTestes/Exercício 019.py
+++ pythonTestes/Exercício 019.py
@@ -1,4 +1,7 @@
 import random
-a = int(input('Digite o número inicial de alunos: '))
-a2 = int(input('Digite o número final de alunos: '))
-print('O aluno escolhido foi: {}'.format(random.randint(a,a2)))
+a1 = input('Digite o 1° alunos: ')
+a2 = input('Digite o 2° alunos: ')
+a3 = input('Digite o 3° alunos: ')
+a4 = input('Digite o 4° alunos: ')
+lista = [a1, a2, a3, a4]
+print('O aluno escolhido foi: {}'.format(random.choice(lista)))

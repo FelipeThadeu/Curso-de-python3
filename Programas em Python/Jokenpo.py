35f45a77ec52fb0195deefb5d24439540d2cf883
Felipe Thadeu <felipet65@gmail.com>
2017-09-25 09:11:00 -0300

    Jokenpo

--- Programas em Python/Jokenpo.py
+++ Programas em Python/Jokenpo.py
@@ -1,7 +1,12 @@
+#Este programa é um mini jogo chamado JOKENPO, tem o objetivo de identificar qual jogador
+#ganhará a depender da escolha
+
 resp = input('Deseja jogar Jokenpo? ')
 while resp.lower() == 'sim':
     jogador1 = input('Jogador 1 deve escolher entre Pedra, Papel e Tesoura: ')
     jogador2 = input('Jogador 2 deve escolher entre Pedra, Papel e Tesoura: ')
+
+    #Jogador 1 quando escolhe Papel
     if jogador1.lower() == 'papel':
         if jogador2.lower() == 'pedra':
             print('Jogador 1 ganhou a partida!')
@@ -11,6 +16,8 @@ while resp.lower() == 'sim':
             print('Jogador 2, você não digitou a opção!')
         else:
             print('Empate! jogue novamente!')
+
+    #Jogador 1 quando escolhe Pedra
     elif jogador1.lower() == 'pedra':
         if jogador2.lower() == 'papel':
             print('Jogador 2 ganhou a partida!')
@@ -20,6 +27,8 @@ while resp.lower() == 'sim':
             print('Jogador 2, você não digitou a opção!')
         else:
             print('Empate! jogue novamente')
+
+    #Jogador 1 quando escolhe Tesoura
     elif jogador1.lower() == 'tesoura':
         if jogador2.lower() == 'pedra':
             print('Jogador 2 ganhou a partida!')
@@ -37,6 +46,8 @@ while resp.lower() == 'sim':
     else:
         print('Não entendi o que você digitou!')
     resp = input('Deseja Jogar novamente? ')
+
+#Opção onde caso o usuário não queira jogar ou digite errado a opção
 else:
     if resp.lower() == 'não':
         print('Obrigado, aguardamos você de volta')
